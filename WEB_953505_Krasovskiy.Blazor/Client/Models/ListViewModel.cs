db2b35c3d0ecd5afc8bdfe51ef73070269d31428
OMWi <bsuiromwi@gmail.com>
2021-11-14 17:03:34 +0300

    add lab8

---
+++ WEB_953505_Krasovskiy.Blazor/Client/Models/ListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WEB_953505_Krasovskiy.Blazor.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("dishId")]
+        public int DishId { get; set; }
+
+        [JsonPropertyName("dishName")]
+        public string DishName { get; set; }
+    }
+}
